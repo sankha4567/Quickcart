34821eb397abe4c22593847dda5ff9ec2ba0afc2
sankha4567 <ssankha103@gmail.com>
2025-08-26 19:54:07 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  _id:{ type : String,requied:true},
+  name:{type : String,requied:true},
+  email:{type : String,requied:true,unique:true},
+  imageUrl:{type : String,requied:true},
+  cartItems:{type:Object,default:{}}
+
+},{minimize:false})
+const User = mongoose.models.user || mongoose.model('user',userSchema);
+export default User;
